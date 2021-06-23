d605596c846a2ba6e13bea2f84ddcd92119e1b77
Vinicius Uehara <viuehara12@gmail.com>
2021-06-23 10:18:28 -0300

    add react lazy pages components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import "normalize.css";
 import "./App.scss";
 import "./styles/main.scss";
-import { Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Account from "./pages/Account";
 import { createBrowserHistory } from "history";
+import { Router, Switch, Route } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+// import Home from "./pages/Home";
+// import Register from "./pages/Register";
+// import Account from "./pages/Account";
+const Home = lazy(() => import("./pages/Home"));
+const Account = lazy(() => import("./pages/Account"));
+const Register = lazy(() => import("./pages/Register"));
 const history = createBrowserHistory();
 
 export default function App() {
     return (
         <Router history={history}>
             <div>
-                <Switch>
-                    <Route path="/register">
-                        <Register />
-                    </Route>
-                    <Route path="/account">
-                        <Account />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch>
+                        <Route path="/register">
+                            <Register />
+                        </Route>
+                        <Route path="/account">
+                            <Account />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Suspense>
             </div>
         </Router>
     );
